105ceb27d54e1bbc3a066c0e62160408482f7843
Rashed Mohammed <25506588+rashedmyt@users.noreply.github.com>
2019-06-19 15:23:34 +0530

    initial splitting of files for modular handlers

--- __init__.py
+++ __init__.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 """This module contains a template MindMeld application"""
-from mindmeld import Application
-
-app = Application(__name__)
+from nlp_intern.root import app
 
 __all__ = ['app']
-
-
-@app.handle(default=True)
-def default(request, responder):
-    """This is a default handler."""
-    responder.reply('Hello there!')
