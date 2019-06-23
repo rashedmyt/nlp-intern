f1f5376e1867e758c630623993677f4ecae9258a
Rashed Mohammed <25506588+rashedmyt@users.noreply.github.com>
2019-06-23 17:54:45 +0530

    added multi_tasking demo code

---
+++ multi_task/main.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import signal
+import sys
+
+def handle_sigint(sig, frame):
+    print('\n\nThank you for using the application.\nClosing now...')
+    sys.exit(0)
+
+URL = 'http://localhost:7150/parse'
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+signal.signal(signal.SIGINT, handle_sigint)
+
+payload = '{"text": "Hi"}'
+
+response = requests.post(url=URL, headers=headers, data=payload)
+
+data = response.json()
+reply = data['directives'][0]['payload']['text']
+
+print('App: ' + reply)
+
+while True:
+    print("\n\nPayload Data:\n" + json.dumps(data))
+    inp = input('You: ')
+
+    payload = '{"text":"%s", "frame":%s}' %(inp,json.dumps(data['frame']))
+
+    response = requests.post(url=URL, headers=headers, data=payload)
+
+    data = response.json()
+
+    reply = data['directives'][0]['payload']['text']
+
+    print('App: ' + reply)
