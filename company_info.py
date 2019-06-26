7c26a92068a3069fd9d60c0a4d047891a5ef0f30
Rashed Mohammed <25506588+rashedmyt@users.noreply.github.com>
2019-06-26 14:53:36 +0530

    shift helpers domain handlers and remaining helper functions to separate file

--- company_info.py
+++ company_info.py
@@ -1,5 +1,6 @@
 from .root import app
 from .root import qa
+from .helpers import handle_highest_salary, handle_last_recruitment
 
 
 @app.handle(intent='last_recruitment')
@@ -10,7 +11,7 @@ def company_last_year(request, responder):
                          for e in request.entities if e['type'] == 'company_name'), company_name)
 
     if company_name:
-        _handle_last_recruitment(company_name, responder)
+        handle_last_recruitment(company_name, responder)
     else:
         responder.frame['desired_action'] = "last_recruitment"
         responder.reply(
@@ -30,7 +31,7 @@ def company_highest_salary(request, responder):
             year = i['value'][0]['value'][0:4]
 
     if company_name and year:
-        _handle_highest_salary(company_name, year, responder)
+        handle_highest_salary(company_name, year, responder)
     elif company_name:
         responder.frame['company_name'] = company_name
         responder.frame['desired_action'] = "highest_salary"
@@ -43,75 +44,4 @@ def company_highest_salary(request, responder):
 
         responder.reply(
             "Sure, which company's highest salary would you like to know?")
-        responder.listen()
-
-
-@app.handle(intent='specify_company')
-def specify_company(request, responder):
-    year = request.frame.get('year')
-    company_name = next((e['value'][0]['cname']
-                         for e in request.entities if e['type'] == 'company_name'), None)
-
-    if company_name:
-        try:
-            if request.frame['desired_action'] == "last_recruitment":
-                _handle_last_recruitment(company_name, responder)
-            elif request.frame['desired_action'] == "highest_salary":
-                _handle_highest_salary(company_name, year, responder)
-        except KeyError:
-            print("please specify some action")
-    else:
-        responder.reply(
-            "Not sure which company you meant there. Can you please try again?")
-
-
-@app.handle(intent='specify_year')
-def specify_year(request, responder):
-    company_name = request.frame.get('company_name')
-    year = next((e['value'][0]['value'][0:4]
-                 for e in request.entities if e['type'] == 'sys_time'), None)
-    print(year)
-    if company_name:
-        try:
-            if request.frame['desired_action'] == "highest_salary":
-                _handle_highest_salary(company_name, year, responder)
-        except KeyError:
-            print("Please specify some action")
-    else:
-        responder.reply(
-            "Not sure which company you meant there. Can you please try again?")
-
-
-# Helpers
-
-
-def _handle_last_recruitment(company_name, responder):
-    company = qa.get(index='companies', name=company_name)[0]
-    last_recruitment_year = sorted(company['data'].keys())[-1]
-
-    responder.frame['company_name'] = company_name
-    responder.frame['year'] = last_recruitment_year
-
-    reply = company_name + " last came for placements in " + last_recruitment_year
-    responder.reply(reply)
-
-
-def _handle_highest_salary(company_name, year, responder):
-    company = qa.get(index='companies', name=company_name)[0]
-
-    responder.frame['company_name'] = company_name
-
-    if year in company['data']:
-        responder.frame['year'] = year
-        highest_salary = company['data'][year]['highest_sal']
-        reply = company_name + " gave a highest salary of " + \
-            highest_salary + " in the year " + year
-        responder.reply(reply)
-    elif year:
-        responder.frame['year'] = year
-        reply = company_name + " didn't came for placements in " + year
-        responder.reply(reply)
-    else:
-        responder.frame['desired_action'] = "highest_salary"
-        responder.reply("Of course, which year?")
         responder.listen()
---
+++ helpers.py
@@ -0,0 +1,73 @@
+from .root import app
+from .root import qa
+
+
+@app.handle(intent='specify_company')
+def specify_company(request, responder):
+    year = request.frame.get('year')
+    company_name = next((e['value'][0]['cname']
+                         for e in request.entities if e['type'] == 'company_name'), None)
+
+    if company_name:
+        try:
+            if request.frame['desired_action'] == "last_recruitment":
+                handle_last_recruitment(company_name, responder)
+            elif request.frame['desired_action'] == "highest_salary":
+                handle_highest_salary(company_name, year, responder)
+        except KeyError:
+            print("please specify some action")
+    else:
+        responder.reply(
+            "Not sure which company you meant there. Can you please try again?")
+
+
+@app.handle(intent='specify_year')
+def specify_year(request, responder):
+    company_name = request.frame.get('company_name')
+    year = next((e['value'][0]['value'][0:4]
+                 for e in request.entities if e['type'] == 'sys_time'), None)
+    print(year)
+    if company_name:
+        try:
+            if request.frame['desired_action'] == "highest_salary":
+                handle_highest_salary(company_name, year, responder)
+        except KeyError:
+            print("Please specify some action")
+    else:
+        responder.reply(
+            "Not sure which company you meant there. Can you please try again?")
+
+
+# Helpers
+
+
+def handle_last_recruitment(company_name, responder):
+    company = qa.get(index='companies', name=company_name)[0]
+    last_recruitment_year = sorted(company['data'].keys())[-1]
+
+    responder.frame['company_name'] = company_name
+    responder.frame['year'] = last_recruitment_year
+
+    reply = company_name + " last came for placements in " + last_recruitment_year
+    responder.reply(reply)
+
+
+def handle_highest_salary(company_name, year, responder):
+    company = qa.get(index='companies', name=company_name)[0]
+
+    responder.frame['company_name'] = company_name
+
+    if year in company['data']:
+        responder.frame['year'] = year
+        highest_salary = company['data'][year]['highest_sal']
+        reply = company_name + " gave a highest salary of " + \
+            highest_salary + " in the year " + year
+        responder.reply(reply)
+    elif year:
+        responder.frame['year'] = year
+        reply = company_name + " didn't came for placements in " + year
+        responder.reply(reply)
+    else:
+        responder.frame['desired_action'] = "highest_salary"
+        responder.reply("Of course, which year?")
+        responder.listen()
